f5f3d7e534ac91497f07c7fdd84b15f8192da79f
puffertron <puffertron@gmail.com>
2018-05-11 16:21:24 -0400

    added more level designs stuffa and fixed coin effect

---
+++ particle systems/coin explosion/coin_explosion.go
@@ -0,0 +1,35 @@
+components {
+  id: "coin_explosion"
+  component: "/particle systems/coin explosion/coin_explosion.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/particle systems/coin explosion/coin_explosion.tilesource\"\n"
+  "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- utility objects/spawners/entityspawner/entity spawner.go
+++ utility objects/spawners/entityspawner/entity spawner.go
@@ -1,3 +1,18 @@
+components {
+  id: "entityspawn"
+  component: "/utility objects/spawners/entityspawner/entityspawn.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "smallcoinfactory"
   type: "factory"
@@ -56,6 +71,24 @@ embedded_components {
   id: "leafparticlefactory"
   type: "factory"
   data: "prototype: \"/particle systems/leaves/leafparticles.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "coineffectfactory"
+  type: "factory"
+  data: "prototype: \"/particle systems/coin explosion/coin_explosion.go\"\n"
   "load_dynamically: false\n"
   ""
   position {
